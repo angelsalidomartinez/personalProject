07e16c5da981598443fa8f6d38f9eee6e1b2b1a5
angel <angel.salido.martinez@gmail.com>
2020-06-26 13:48:02 +0200

    Expire the token in ten 10

--- src/main/java/usecases/userManagement/application/services/TokenService.java
+++ src/main/java/usecases/userManagement/application/services/TokenService.java
@@ -7,6 +7,11 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 @Service
 public class TokenService {
 
@@ -15,7 +20,9 @@ public class TokenService {
     String create(){
         String token = "";
         try{
-            token = JWT.create().withIssuer("auth0").sign(algorithmToSignToken);
+            LocalDate expirationTime = LocalDateTime.now().plusDays(10).toLocalDate();
+            Date expirationDate = Date.from(expirationTime.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+            token = JWT.create().withIssuer("auth0").withExpiresAt(expirationDate).sign(algorithmToSignToken);
         }catch(JWTCreationException jwtCreationException){
 
         }
@@ -27,5 +34,4 @@ public class TokenService {
         DecodedJWT decodedJWT = verifier.verify(token);
         return decodedJWT != null;
     }
-
 }
