1c9869019da3cbfd1962b48f23c0543dab5f4522
angel <angel.salido.martinez@gmail.com>
2020-08-07 16:00:11 +0200

    expiration flow

--- src/main/java/usecases/userManagement/infrastructure/controllers/UserController.java
+++ src/main/java/usecases/userManagement/infrastructure/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import usecases.userManagement.infrastructure.dto.User;
 import usecases.userManagement.infrastructure.exceptions.UserAutenticationException;
 import usecases.userManagement.infrastructure.exceptions.UserCreationException;
+import usecases.userManagement.infrastructure.exceptions.UserExpirationException;
 
 import javax.validation.Valid;
 
@@ -24,6 +25,10 @@ public class UserController {
     @PutMapping("/registerUser")
     public User registerUser(@Valid @RequestBody User user) throws UserCreationException {
         return userService.registerUser(user);
+    }
+
+    public boolean logout(@RequestParam(value = "email") String email) throws UserExpirationException {
+        return userService.logout(email);
     }
 
     public void setUserService(UserService userService) {
